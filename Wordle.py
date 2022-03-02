f96e1e10be048d4291b225b3990ea414a8147d8c
nerdgod21 <56525761+nerdgod21@users.noreply.github.com>
2022-03-01 21:51:07 -0800

    Update Wordle.py

--- Wordle.py
+++ Wordle.py
@@ -1,13 +1,15 @@
-import random
-wordlist, v = ("doofs","dylan","doodo"), 0
+import numpy as np
+import random 
+filename = 'wordlist.txt'
+wordlist, v = np.loadtxt(filename, skiprows=0, dtype=str), 0
 word = random.choice(wordlist)
+print(word)
 while v < 6:
     wordle_guess = input('--> ') 
     if word == wordle_guess:
         print(f'Yay you did it!!')
     else:
-        v += 1
-    
+        v += 1 
     for i in range(len(word)):
         if (wordle_guess[i] == word[i]):
             print(i+1)
